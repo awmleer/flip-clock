5ed52404e930a906f6e694486d08f81639af4f64
awmleer <awmleer@sparker.xyz>
2017-08-27 14:28:35 +0800

    [*] css rotate test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,8 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
-  }
+  constructor(
+    public navCtrl: NavController
+  ) {}
 
 }
